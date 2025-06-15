ab1c984ebfd32f06357f6580a77a42f2029851e1
Will Masters <willmasters@users.noreply.github.com>
2025-06-15 10:58:26 +0000

    Update: fix  speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { SpeedInsights } from '@vercel/speed-insights/svelte';
+	import SpeedInsights from '@vercel/speed-insights/svelte/dist/index.svelte';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from 'svelte-sonner';
 	import '../app.postcss';
